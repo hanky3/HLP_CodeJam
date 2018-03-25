2c6df705ba5318571f30a379662e0a6285d190e0
hanky3 <34890345+hanky3@users.noreply.github.com>
2018-03-26 01:48:45 +0900

    update code - FOSSIL

--- Practice/WEEK_XX/CH13_FOSSIL.cpp
+++ Practice/WEEK_XX/CH13_FOSSIL.cpp
@@ -39,12 +39,6 @@ typedef vector<pair<HullPoint, HullPoint>> HullLines;
 HullLines hullUpper;
 HullLines hullDown;
 
-double ccw(HullPoint p, HullPoint a, HullPoint b)
-{
-	double ret = (b.x - p.x) * (p.y - a.y) + (b.y - p.y) * (a.x - p.x);
-	return ret;
-}
-
 void readInputData()
 {
 	int pointACnt, pointBCnt;
@@ -102,123 +96,6 @@ void makeHull(vector<HullPoint> &points, HullLines &upper, HullLines &down)
 }
 
 
-inline double Det(double a, double b, double c, double d)
-{
-	return a*d - b*c;
-}
-
-///Calculate intersection of two lines.
-///\return true if found, false if not found or error
-//bool LineLineIntersect(double x1, double y1, //Line 1 start
-//	double x2, double y2, //Line 1 end
-//	double x3, double y3, //Line 2 start
-//	double x4, double y4, //Line 2 end
-//	double &ixOut, double &iyOut) //Output 
-bool lineIntersected(pair<HullPoint, HullPoint> lineA, pair<HullPoint, HullPoint> lineB, HullPoint &interP)
-{
-	double detL1 = Det(lineA.first.x, lineA.first.y, lineA.second.x, lineA.second.y);
-	double detL2 = Det(lineB.first.x, lineB.first.y, lineB.second.x, lineB.second.y);
-	double x1mx2 = lineA.first.x - lineA.second.x;
-	double x3mx4 = lineB.first.x - lineB.second.x;
-	double y1my2 = lineA.first.y - lineA.second.y;
-	double y3my4 = lineB.first.y - lineB.second.y;
-
-	double xnom = Det(detL1, x1mx2, detL2, x3mx4);
-	double ynom = Det(detL1, y1my2, detL2, y3my4);
-	double denom = Det(x1mx2, y1my2, x3mx4, y3my4);
-	if (denom == 0.0)//Lines don't seem to cross
-	{
-		interP.x = NAN;
-		interP.y = NAN;
-		return false;
-	}
-
-	interP.x = xnom / denom;
-	interP.y = ynom / denom;
-	if (!isfinite(interP.x) || !isfinite(interP.y)) //Probably a numerical issue
-		return false;
-
-	return true; //All OK
-}
-
-#if 0
-bool lineIntersected(pair<HullPoint, HullPoint> lineA, pair<HullPoint, HullPoint> lineB, HullPoint &interP)
-{
-	bool checked = false;
-	double x, y;
-
-	// 流急老 版快 眉农
-	if (lineA.first.x == lineA.second.x)
-		x = lineA.first.x;
-
-	if (!checked) {
-		double a1 = (lineA.first.y - lineA.second.y) / (lineA.first.x - lineA.second.x);
-		double b1 = lineA.first.y - (a1 * lineA.first.x);
-		double a2 = (lineB.first.y - lineB.second.y) / (lineB.first.x - lineB.second.x);
-		double b2 = lineB.first.y - (a2 * lineB.first.x);
-
-		x = (b2 - b1) / (a1 - a2);
-		y = a1 * x + b1;
-	}
-
-
-	interP.x = x; interP.y = y;
-	// range check for A
-	if (x < min(lineA.first.x, lineA.second.x) || x > max(lineA.first.x, lineA.second.x))
-		return false;
-	if (y < min(lineA.first.y, lineA.second.y) || y > max(lineA.first.y, lineA.second.y))
-		return false;
-
-	// range check for B
-	if (x < min(lineB.first.x, lineB.second.x) || x > max(lineB.first.x, lineB.second.x))
-		return false;
-	if (y < min(lineB.first.y, lineB.second.y) || y > max(lineB.first.y, lineB.second.y))
-		return false;
-
-
-	return true;
-}
-#endif
-
-bool isHullInside(vector<HullPoint> &a, vector<HullPoint> &b)
-{
-	for (auto pointA : a) {
-		for (int curr = 0; curr < b.size(); curr++) {
-			int next = (curr + 1) % b.size();
-			if (ccw(b[curr], b[next], pointA) < 0)
-				return false;
-		}
-	}
-
-	return true;
-}
-
-bool findIntersections(HullPoint &min, HullPoint &max)
-{
-	bool ret = false;
-	min.x = 101; min.y = 101;
-	max.x = -1; max.y = -1;
-	for (int a = 0; a < pointA.size(); a++) {
-		pair<HullPoint, HullPoint> lineA;
-		lineA.first = pointA[a];
-		lineA.second = pointA[(a+1)%pointA.size()];
-
-		for (int b = 0; b < pointB.size(); b++) {
-			pair<HullPoint, HullPoint> lineB;
-			lineB.first = pointB[b];
-			lineB.second = pointB[(b + 1) % pointB.size()];
-		
-			HullPoint p;
-			if (lineIntersected(lineA, lineB, p)) {
-				min = (p.x < min.x) ? p : min;
-				max = (p.x > max.x) ? p : max;
-				ret = true;
-			}
-		}
-	}
-	return ret;
-}
-
 double findY(bool minFlag, double x, HullLines &hulls)
 {
 	double ret = 0;
@@ -226,6 +103,7 @@ double findY(bool minFlag, double x, HullLines &hulls)
 	if (minFlag)
 		ret = numeric_limits<double>::max();
 
+	bool intersected = false;
 	for (auto hullLine : hulls) {
 		if (x < hullLine.first.x || x > hullLine.second.x)
 			continue;
@@ -238,7 +116,11 @@ double findY(bool minFlag, double x, HullLines &hulls)
 			ret = min(ret, y);
 		else
 			ret = max(ret, y);
+		
+		intersected = true;
 	}
+	if (!intersected || ret < 0)
+		ret = 0.0f;
 
 	return ret;
 }
@@ -248,26 +130,12 @@ double maxOverlapVertical()
 	double ret = 0.0f;
 	HullPoint minP, maxP;
 
-	if (findIntersections(minP, maxP)) {
-		makeHull(pointA, hullUpper, hullDown);
-		makeHull(pointB, hullUpper, hullDown);
-	}
-	else {
-		if (isHullInside(pointA, pointB)) {
-			makeHull(pointB, hullUpper, hullDown);
-		}
-		else if (isHullInside(pointB, pointA)) {
-			makeHull(pointB, hullUpper, hullDown);
-		}
-		else
-			return 0.0f;
+	makeHull(pointA, hullUpper, hullDown);
+	makeHull(pointB, hullUpper, hullDown);
+	
+	double loX = max(min_element(pointA.begin(), pointA.end())->x, min_element(pointB.begin(), pointB.end())->x);
+	double hiX = min(max_element(pointA.begin(), pointA.end())->x, max_element(pointB.begin(), pointB.end())->x);
 
-		minP = hullUpper[0].first;
-		maxP = hullUpper[hullUpper.size() - 1].second;
-	}
-
-	double loX = minP.x;
-	double hiX = maxP.x;
 	for (int iter = 0; iter < 100; iter++) {
 		double x1 = (2*loX + hiX)  / 3;
 		double x2 = (loX + 2*hiX) / 3;
@@ -284,6 +152,8 @@ double maxOverlapVertical()
 		else
 			hiX = x2;
 	}
+	if (ret < 0)
+		ret = 0.0f;
 
 	return ret;
 }
